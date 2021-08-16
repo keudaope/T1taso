7de2376152c9e5ac99687dd3b18857393066b2e5
jyri.lindroos@keuda.fi <jyri.lindroos@keuda.fi>
2021-08-16 07:51:35 +0300

    ökljöl

---
+++ T1taso/T1taso/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T1taso
+{
+    public partial class T1Taso : Form
+    {
+        public T1Taso()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
